e533c43bb3d7a3121a42b7b55604468fb0e906bc
alumno <alumno>
2017-03-16 21:53:44 -0300

    primer commit

---
+++ app/src/main/java/com/example/alumno/miprimeraapps/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.alumno.miprimeraapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv  = (TextView) findViewById( R.id.tv );
+        tv.setText("HOla mono");
+
+}}
